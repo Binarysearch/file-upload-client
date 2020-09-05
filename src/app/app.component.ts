df3d24ae65cab31b43b4b33ef4e9b638efc460b4
Binarysearch <desenlace.net@gmail.com>
2020-09-05 21:20:41 +0200

    mas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'file-upload-client';
+
+  SERVER_URL = "http://localhost:3000/upload";
+  file: File;
+
+  images: { name: string; }[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    
+  }
+
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.file);
+
+    this.httpClient.post<{ name: string; }>(this.SERVER_URL, formData).subscribe(
+      (res) => {
+        this.images.push(res);
+      },
+      (err) => console.log(err)
+    );
+  }
 }
